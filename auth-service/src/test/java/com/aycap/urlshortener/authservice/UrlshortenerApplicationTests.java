7d1d11310dd0cb37a753d1b7d4ed9dbbbdd98aca
Apichon Changkong <apichon.cha@hotmail.com>
2026-09-18 18:27:39 +0700

    feat(auth-service): init auth-service project starter kit

---
+++ auth-service/src/test/java/com/aycap/urlshortener/authservice/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aycap.urlshortener.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
